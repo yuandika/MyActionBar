1ac019e659d501e0d8ed4661de4a1024a764f52d
Yuandika Alfahreiza <yuandika11@gmail.com>
2021-12-07 23:44:28 +0700

    init commit

---
+++ app/src/main/java/com/dicoding/myactionbar/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.dicoding.myactionbar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu1 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_countainer, MenuFragment())
+                    .addToBackStack(null)
+                    .commit()
+                return true
+            }
+            R.id.menu2 -> {
+                val i = Intent(this, MenuActivity::class.java)
+                startActivity(i)
+                return true
+            }
+            else -> return true
+        }
+    }
+}
